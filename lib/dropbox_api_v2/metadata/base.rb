f219369779d141a8e0ca82158725c134049fe3fb
Jesús Burgos <jesus@visrez.com>
2016-05-01 02:25:34 +0200

    Use class instance variables instead of class variables. Fixes a bug.

--- lib/dropbox_api_v2/metadata/base.rb
+++ lib/dropbox_api_v2/metadata/base.rb
@@ -1,20 +1,25 @@
 module DropboxApiV2::Metadata
   class Base
-    @@fields = {}
+    def self.field(name, type)
+      @fields ||= {}
+      @fields[name] = type
+
+      attr_reader name
+    end
+
+    def self.each_field
+      @fields.each do |name, type|
+        yield name, type
+      end
+    end
 
     def initialize(metadata)
-      @@fields.each do |field, type|
-        instance_variable_set "@#{field}", cast(metadata[field.to_s], type)
+      self.class.each_field do |name, type|
+        instance_variable_set "@#{name}", cast(metadata[name.to_s], type)
       end
     end
 
     private
-
-    def self.field(name, type)
-      @@fields[name] = type
-
-      attr_reader name
-    end
 
     def cast(object, type)
       if type == String
