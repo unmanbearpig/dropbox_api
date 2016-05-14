380514244df998b1bddacc2ddaa9d32fec818ef7
Jesús Burgos <jesus@visrez.com>
2016-05-14 11:29:02 +0200

    Use single quotes for requires...

--- lib/dropbox_api_v2.rb
+++ lib/dropbox_api_v2.rb
@@ -1,27 +1,29 @@
+require 'dropbox_api_v2/version'
+
 require 'json'
 require 'faraday'
 
-require "dropbox_api_v2/middleware/decode_result"
-require "dropbox_api_v2/middleware/encode_args_in_body"
-require "dropbox_api_v2/middleware/encode_args_in_headers"
+require 'dropbox_api_v2/middleware/decode_result'
+require 'dropbox_api_v2/middleware/encode_args_in_body'
+require 'dropbox_api_v2/middleware/encode_args_in_headers'
 
-require "dropbox_api_v2/metadata/base"
-require "dropbox_api_v2/metadata/file"
-require "dropbox_api_v2/metadata/folder"
-require "dropbox_api_v2/metadata/factory"
+require 'dropbox_api_v2/metadata/base'
+require 'dropbox_api_v2/metadata/file'
+require 'dropbox_api_v2/metadata/folder'
+require 'dropbox_api_v2/metadata/factory'
 
-require "dropbox_api_v2/errors/basic_error"
-require "dropbox_api_v2/errors/lookup_error"
-require "dropbox_api_v2/errors/write_conflict_error"
-require "dropbox_api_v2/errors/write_error"
-require "dropbox_api_v2/errors/relocation_error"
-require "dropbox_api_v2/errors/create_folder_error"
-require "dropbox_api_v2/errors/delete_error"
-require "dropbox_api_v2/errors/download_error"
+require 'dropbox_api_v2/errors/basic_error'
+require 'dropbox_api_v2/errors/lookup_error'
+require 'dropbox_api_v2/errors/write_conflict_error'
+require 'dropbox_api_v2/errors/write_error'
+require 'dropbox_api_v2/errors/relocation_error'
+require 'dropbox_api_v2/errors/create_folder_error'
+require 'dropbox_api_v2/errors/delete_error'
+require 'dropbox_api_v2/errors/download_error'
 
-require "dropbox_api_v2/connection_builder"
-require "dropbox_api_v2/client"
-require "dropbox_api_v2/response"
+require 'dropbox_api_v2/api_result'
+require 'dropbox_api_v2/client'
+require 'dropbox_api_v2/connection_builder'
 
 require 'dropbox_api_v2/endpoints/base'
 require 'dropbox_api_v2/endpoints/rpc'
@@ -31,5 +33,3 @@ require 'dropbox_api_v2/endpoints/files/copy'
 require 'dropbox_api_v2/endpoints/files/create_folder'
 require 'dropbox_api_v2/endpoints/files/delete'
 require 'dropbox_api_v2/endpoints/files/download'
-
-require "dropbox_api_v2/version"
