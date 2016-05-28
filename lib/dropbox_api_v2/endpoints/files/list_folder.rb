98e8de5cde175f707ac8b73b3ab0c0c1b10ad8f2
Jesús Burgos <jesus@visrez.com>
2016-05-28 12:27:01 +0200

    Adds `list_folder` endpoint.

---
+++ lib/dropbox_api_v2/endpoints/files/list_folder.rb
@@ -0,0 +1,48 @@
+module DropboxApiV2::Endpoints::Files
+  class ListFolder < DropboxApiV2::Endpoints::Rpc
+    Method      = :post
+    Path        = "/2/files/list_folder".freeze
+    ResultType  = DropboxApiV2::Results::ListFolderResult
+    ErrorType   = DropboxApiV2::Errors::ListFolderError
+
+    # @method list_folder(path, options = {})
+    # Returns the contents of a folder.
+    #
+    # @param path [String] The path to the folder you want to read.
+    # @option recursive [Boolean] If `true`, the list folder operation will be
+    #   applied recursively to all subfolders and the response will contain
+    #   contents of all subfolders. The default for this field is `false`.
+    # @option include_media_info [Boolean] If `true`, FileMetadata.media_info
+    #   is set for photo and video. The default for this field is `false`.
+    # @option include_deleted [Boolean] If `true`, DeletedMetadata will be
+    #   returned for deleted file or folder, otherwise LookupError.not_found
+    #   will be returned. The default for this field is False.
+    add_endpoint :list_folder do |path, options = {}|
+      validate_options(options)
+      options[:recursive] ||= false
+      options[:include_media_info] ||= false
+      options[:include_deleted] ||= false
+
+      perform_request options.merge({
+        :path => path
+      })
+    end
+
+    private
+
+    def validate_options(options)
+      valid_option_keys = %i(
+        recursive
+        include_media_info
+        include_deleted
+        include_has_explicit_shared_members
+      )
+
+      options.keys.each do |key|
+        unless valid_option_keys.include? key.to_sym
+          raise ArgumentError, "Invalid option `#{key}`"
+        end
+      end
+    end
+  end
+end
--- spec/endpoints/files_spec.rb
+++ spec/endpoints/files_spec.rb
@@ -175,4 +175,32 @@ context DropboxApiV2::Endpoints::Files do
       }.to raise_error(ArgumentError)
     end
   end
+
+  describe "#list_folder" do
+    it "returns a ListFolderResult", :cassette => "list_folder/success" do
+      result = @client.list_folder ""
+
+      expect(result).to be_a(DropboxApiV2::Results::ListFolderResult)
+    end
+
+    it "raises an error if the file can't be found", :cassette => "list_folder/not_found" do
+      expect {
+        @client.get_thumbnail "/unexisting_folder"
+      }.to raise_error(DropboxApiV2::Errors::NotFoundError)
+    end
+
+    it "returns all entries as metadata objects", :cassette => "list_folder/success" do
+      result = @client.list_folder ""
+
+      result.entries.each do |resource|
+        expect(resource).to be_a(DropboxApiV2::Metadata::Base)
+      end
+    end
+
+    it "raises an argument error with invalid options" do
+      expect {
+        @client.list_folder "/img.png", :invalid_arg => "value"
+      }.to raise_error(ArgumentError)
+    end
+  end
 end
