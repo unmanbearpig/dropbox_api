a3b06cfa3ed94258579f8277ea039d2bb44bdb31
Jesús Burgos <jesus@visrez.com>
2016-05-28 11:39:08 +0200

    Removes old implementation of `get_thumbnail`.

--- lib/dropbox_api_v2/endpoints/files.rb
+++ lib/dropbox_api_v2/endpoints/files.rb
@@ -1,29 +1,5 @@
 module DropboxApiV2::Endpoints
   module Files
-    # Get a thumbnail for an image.
-    #
-    # This method currently supports files with the following file extensions:
-    # jpg, jpeg, png, tiff, tif, gif and bmp. Photos that are larger than 20MB
-    # in size won't be converted to a thumbnail.
-    #
-    # @param path [String] The path to the image file you want to thumbnail.
-    # @option format [:jpg, :png] The format for the thumbnail image, jpeg
-    #   (default) or png. For images that are photos, jpeg should be preferred,
-    #   while png is better for screenshots and digital arts. The default is
-    #   :jpeg.
-    # @option size [:w32h32, :w64h64, :w128h128, :w640h480, :w1024h768] The
-    #   size for the thumbnail image. The default is :w64h64
-    def get_thumbnail(path, options = {})
-      options[:format] ||= :jpg
-      options[:size] ||= :w64h64
-
-      request :post, "/2/files/get_thumbnail", {
-        :path => path,
-        :format => options[:format],
-        :size => options[:size]
-      }
-    end
-
     # Returns the contents of a folder.
     #
     # @param path [String] The path to the folder you want to read.
