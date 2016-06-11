ca23110fa0a62b2ed7ade3a90e0ecf7f1a0e625c
Jesús Burgos <jesus@visrez.com>
2016-06-11 12:03:19 +0200

    Fix fixture names.

--- spec/endpoints/sharing_spec.rb
+++ spec/endpoints/sharing_spec.rb
@@ -4,23 +4,23 @@ context DropboxApiV2::Endpoints::Sharing do
   end
 
   describe "#share_folder" do
-    it "returns the shared folder", :cassette => "shared_folder/success" do
-      folder = @client.share_folder("/folder_b")
+    it "returns the shared folder", :cassette => "share_folder/success" do
+      folder = @client.share_folder("/folder_k")
 
       expect(folder).to be_a(DropboxApiV2::Metadata::SharedFolder)
     end
 
-    it "returns the shared folder", :cassette => "shared_folder/bad_path" do
+    it "returns the shared folder", :cassette => "share_folder/bad_path" do
       expect {
-        @client.share_folder("/folder_b")
+        @client.share_folder("/test_folder")
       }.to raise_error(DropboxApiV2::Errors::BadPathError)
     end
 
-    it "contains a shared folder id", :cassette => "shared_folder/success" do
-      folder = @client.share_folder("/folder_b")
+    it "contains a shared folder id", :cassette => "share_folder/success" do
+      folder = @client.share_folder("/folder_k")
 
       expect(folder.shared_folder_id.to_s)
-        .to eq("1236358158") # taken from fixture (shared_folder/success.yml)
+        .to eq("1236414195") # taken from fixture (share_folder/success.yml)
     end
   end
 
@@ -43,7 +43,7 @@ context DropboxApiV2::Endpoints::Sharing do
       # The endpoint doesn't have any return values, can't test the output!
     end
 
-    it "fails with an invalid folder id", :cassette => "shared_folder/invalid_folder" do
+    it "fails with an invalid folder id", :cassette => "add_folder_member/invalid_folder" do
       folder_id = "xxx"
       expect {
         folder = @client.add_folder_member(folder_id, [
