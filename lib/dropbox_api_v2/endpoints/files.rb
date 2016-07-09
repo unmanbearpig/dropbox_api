959f9103971ec96451c8a59787d98771acbe517a
Jesús Burgos <jesus@visrez.com>
2016-07-09 09:02:57 +0200

    Removes deprecated implementation of `search` endpoint.

--- lib/dropbox_api_v2/endpoints/files.rb
+++ lib/dropbox_api_v2/endpoints/files.rb
@@ -77,36 +77,5 @@ module DropboxApiV2::Endpoints
         :rev => rev
       }
     end
-
-    # Searches for files and folders.
-    #
-    # Note: Recent changes may not immediately be reflected in search results
-    # due to a short delay in indexing.
-    #
-    # @param query [String] The string to search for. The search string is
-    #   split on spaces into multiple tokens. For file name searching, the last
-    #   token is used for prefix matching (i.e. "bat c" matches "bat cave" but
-    #   not "batman car").
-    # @param path [String] The path in the user's Dropbox to search.
-    # @option start [Numeric] The starting index within the search results
-    #   (used for paging). The default for this field is 0.
-    # @option max_results [Numeric] The maximum number of search results to
-    #   return. The default for this field is 100.
-    # @option mode [:filename, :filename_and_content, :deleted_filename] The
-    #   search mode. Note that searching file content is only available for
-    #   Dropbox Business accounts. The default is filename.
-    def search(query, path = "", options = {})
-      options[:start] ||= 0
-      options[:max_results] ||= 100
-      options[:mode] ||= :filename
-
-      request :post, "/2/files/search", {
-        :path => path,
-        :query => query,
-        :start => options[:start],
-        :max_results => options[:max_results],
-        :mode => options[:mode]
-      }
-    end
   end
 end
