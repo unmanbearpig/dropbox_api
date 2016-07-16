068f6e212ce411958edc00cd5272a37e69239514
Jesús Burgos <jesus@visrez.com>
2016-07-16 12:04:02 +0200

    Improves the error message when we get an HTTP error response.

--- lib/dropbox_api_v2/endpoints/base.rb
+++ lib/dropbox_api_v2/endpoints/base.rb
@@ -22,7 +22,7 @@ module DropboxApiV2::Endpoints
         # the response body to build an exception.
         build_result(raw_response.env[:api_result])
       else
-        raise raw_response.body
+        raise "Error #{raw_response.status}: #{raw_response.body}"
       end
     end
 
