197bf0b07c5cbad211b387b9599c94e8a2650f29
Jesús Burgos <jesus@visrez.com>
2016-12-06 13:00:29 +0100

    Break everything!
    Many cassettes were using outdated requests. These need to be updated,
    it's better to have broken tests than those which just seem to be
    working but have wrong fixtures.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,6 +3,9 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock # or :fakeweb
+  config.default_cassette_options = {
+    :match_requests_on => [:method, :uri, :body_as_json]
+  }
 end
 
 RSpec.configure do |c|
