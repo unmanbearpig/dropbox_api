bc272671d7db2c17f6e2a9b040c8f97a74c7a78f
Jesús Burgos <jesus@visrez.com>
2017-01-07 17:49:25 +0100

    Removes redundant tests.

--- spec/endpoints/files_spec.rb
+++ spec/endpoints/files_spec.rb
@@ -3,27 +3,6 @@ context DropboxApi::Endpoints::Files do
     @client = DropboxApi::Client.new
   end
 
-  describe "#copy" do
-    it "returns the copied file on success", :cassette => "copy/success" do
-      file = @client.copy("/a.jpg", "/b.jpg")
-
-      expect(file).to be_a(DropboxApi::Metadata::File)
-      expect(file.name).to eq("b.jpg")
-    end
-
-    it "raises an error if the file can't be found", :cassette => "copy/not_found" do
-      expect {
-        @client.copy("/z.jpg", "/b.jpg")
-      }.to raise_error(DropboxApi::Errors::NotFoundError)
-    end
-
-    it "raises an error if the path is invalid", :cassette => "copy/malformed_path" do
-      expect {
-        @client.copy("/../invalid_path.jpg", "/b.jpg")
-      }.to raise_error(DropboxApi::Errors::MalformedPathError)
-    end
-  end
-
   describe "#create_folder" do
     it "returns the new folder on success", :cassette => "create_folder/success" do
       folder = @client.create_folder("/arizona_baby")
