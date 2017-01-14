ddb9d7fe0d988be355d4c2e6638be7359303ce8b
Jesús Burgos <jesus@visrez.com>
2017-01-14 15:22:24 +0100

    Ignore some headers in VCR, trying to pass tests.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,8 +3,20 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock # or :fakeweb
+
+  headers_matcher =
   config.default_cassette_options = {
-    :match_requests_on => [:method, :uri, :body, :headers]
+    :match_requests_on => [
+      :method,
+      :uri,
+      :body,
+      lambda { |a, b|
+        [
+          "Authorization",
+          "Dropbox-Api-Arg"
+        ].all? { |header| a.headers[header] == b.headers[header] }
+      }
+    ]
   }
 end
 
