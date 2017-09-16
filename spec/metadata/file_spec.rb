a620633c5b3b7984193e0c9629afa4a2958558f1
Jesús Burgos <jesus@visrez.com>
2017-09-16 10:57:12 +0200

    Updates tests, trying to be consistent

--- spec/metadata/file_spec.rb
+++ spec/metadata/file_spec.rb
@@ -1,7 +1,5 @@
 context DropboxApi::Metadata::File do
-  let(:content_hash) { nil }
-  let(:media_info) { nil }
-  let(:file_hash) do
+  def build_file_hash(attrs = {})
     {
       ".tag" => "file",
       "name" => "visrez-video.mp4",
@@ -11,74 +9,126 @@ context DropboxApi::Metadata::File do
       "client_modified" => "2016-05-22T09:31:26Z",
       "server_modified" => "2016-05-22T09:31:26Z",
       "rev" => "2c824061bdd",
-      "size" => 1136802,
-      "content_hash" => content_hash,
-      "media_info" => media_info
-    }
+      "size" => 1136802
+    }.merge(attrs)
   end
 
-  subject { DropboxApi::Metadata::File.new(file_hash) }
+  it "can be initialized from a hash" do
+    file_hash = build_file_hash
 
-  describe '.new' do
-    context 'can be initialized from a hash' do
-      it 'works' do
-        expect(subject).to be_a(DropboxApi::Metadata::File)
-        expect(subject.name).to eq("visrez-video.mp4")
-        expect(subject.path_lower).to eq("/visrez-video.mp4")
-        expect(subject.path_display).to eq("/visrez-video.mp4")
-        expect(subject.id).to eq("id:ARSNw7QJjrAAAAAAAAAAAQ")
-        expect(subject.client_modified).to eq(Time.utc(2016, 5, 22, 9, 31, 26))
-        expect(subject.server_modified).to eq(Time.utc(2016, 5, 22, 9, 31, 26))
-        expect(subject.rev).to eq("2c824061bdd")
-        expect(subject.size).to eq(1136802)
-      end
-    end
+    file = DropboxApi::Metadata::File.new file_hash
+    expect(file).to be_a(DropboxApi::Metadata::File)
+    expect(file.name).to eq("visrez-video.mp4")
+    expect(file.path_lower).to eq("/visrez-video.mp4")
+    expect(file.path_display).to eq("/visrez-video.mp4")
+    expect(file.id).to eq("id:ARSNw7QJjrAAAAAAAAAAAQ")
+    expect(file.client_modified).to eq(Time.utc(2016, 5, 22, 9, 31, 26))
+    expect(file.server_modified).to eq(Time.utc(2016, 5, 22, 9, 31, 26))
+    expect(file.rev).to eq("2c824061bdd")
+    expect(file.size).to eq(1136802)
+  end
 
-    context 'includes the content_hash field' do
-      let(:content_hash) { '2a846fa617c3361fc117e1c5c1e1838c336b6a5cef982c1a2d9bdf68f2f1992a' }
+  it "includes the content_hash field" do
+    file_hash = build_file_hash({
+      "content_hash" =>
+        "2a846fa617c3361fc117e1c5c1e1838c336b6a5cef982c1a2d9bdf68f2f1992a"
+    })
 
-      it 'works' do
-        expect(subject).to be_a(DropboxApi::Metadata::File)
-        expect(subject.content_hash).to eq('2a846fa617c3361fc117e1c5c1e1838c336b6a5cef982c1a2d9bdf68f2f1992a')
-      end
-    end
+    file = DropboxApi::Metadata::File.new file_hash
+    expect(file).to be_a(DropboxApi::Metadata::File)
+    expect(file.content_hash).to eq("2a846fa617c3361fc117e1c5c1e1838c336b6a5cef982c1a2d9bdf68f2f1992a")
+  end
 
-    context "includes the media_info field" do
-      let(:media_info) do
-        {
+  context "with photo" do
+    it "includes the media_info field" do
+      file_hash = build_file_hash({
+        "media_info" => {
           ".tag" => "metadata",
           "metadata" => {
-            ".tag" => type,
-            "dimensions" => { "height" => 700, "width" => 1050 },
+            ".tag" => "photo",
+            "dimensions" => {
+              "height" => 700,
+              "width" => 1050
+            },
             "time_taken" => "2013-01-10T22:10:13Z",
-            "location" => { "latitude" => 10.1234, "longitude" => 5.1234 }
+            "location" => {
+              "latitude" => 10.1234,
+              "longitude" => 5.1234
+            }
           }
         }
-      end
+      })
 
-      context 'photo' do
-        let(:type) { 'photo' }
-        it 'works' do
-          expect(subject).to be_a(DropboxApi::Metadata::File)
-          expect(subject.media_info).to be_a(DropboxApi::Metadata::PhotoMetadata)
-          expect(subject.media_info.dimensions).to be_a(DropboxApi::Metadata::Dimensions)
-          expect(subject.media_info.location).to be_a(DropboxApi::Metadata::Location)
-        end
-      end
+      file = DropboxApi::Metadata::File.new file_hash
 
-      context 'video' do
-        let(:type) { 'video' }
-        it 'works' do
-          expect(subject).to be_a(DropboxApi::Metadata::File)
-          expect(subject.media_info).to be_a(DropboxApi::Metadata::VideoMetadata)
-          expect(subject.media_info.dimensions).to be_a(DropboxApi::Metadata::Dimensions)
-          expect(subject.media_info.location).to be_a(DropboxApi::Metadata::Location)
-        end
-      end
+      expect(file)
+        .to be_a(DropboxApi::Metadata::File)
+      expect(file.media_info)
+        .to be_a(DropboxApi::Metadata::PhotoMetadata)
+
+      expect(file.media_info.dimensions)
+        .to be_a(DropboxApi::Metadata::Dimensions)
+      expect(file.media_info.dimensions.height)
+        .to eq(700)
+      expect(file.media_info.dimensions.width)
+        .to eq(1050)
+
+      expect(file.media_info.time_taken)
+        .to eq(Time.utc(2013, 1, 10, 22, 10, 13))
+
+      expect(file.media_info.location)
+        .to be_a(DropboxApi::Metadata::Location)
+      expect(file.media_info.location.latitude)
+        .to eq(10.1234)
+      expect(file.media_info.location.longitude)
+        .to eq(5.1234)
+    end
+  end
+
+  context "with video" do
+    it "includes the media_info field" do
+      file_hash = build_file_hash({
+        "media_info" => {
+          ".tag" => "metadata",
+          "metadata" => {
+            ".tag" => "video",
+            "dimensions" => {
+              "height" => 1920,
+              "width" => 1080
+            },
+            "time_taken" => "2017-07-10T19:54:57Z",
+            "duration" => 12642
+          }
+        }
+      })
+
+      file = DropboxApi::Metadata::File.new file_hash
+
+      expect(file)
+        .to be_a(DropboxApi::Metadata::File)
+      expect(file.media_info)
+        .to be_a(DropboxApi::Metadata::VideoMetadata)
+
+      expect(file.media_info.dimensions)
+        .to be_a(DropboxApi::Metadata::Dimensions)
+      expect(file.media_info.dimensions.height)
+        .to eq(1920)
+      expect(file.media_info.dimensions.width)
+        .to eq(1080)
+
+      expect(file.media_info.time_taken)
+        .to eq(Time.utc(2017, 7, 10, 19, 54, 57))
+
+      expect(file.media_info.duration)
+        .to eq(12642)
     end
   end
 
   it "can be converted back to a hash" do
-    expect(subject.to_hash).to eq(file_hash.delete_if { |k, v| v.nil? })
+    file_hash = build_file_hash
+
+    file = DropboxApi::Metadata::File.new file_hash
+
+    expect(file.to_hash).to eq(file_hash)
   end
 end
