84288b08d281730eacdb4b4384f38dd927ec84e0
John Royall <johnroyall7@yahoo.com>
2018-02-06 13:39:49 -0500

    add `get_thumbnail_batch` spec

---
+++ spec/endpoints/files/get_thumbnail_batch_spec.rb
@@ -0,0 +1,25 @@
+describe DropboxApi::Client, "#get_thumbnail_batch" do
+  before :each do
+    @client = DropboxApi::Client.new
+  end
+
+  it "returns files", :cassette => "get_thumbnail_batch/success" do
+    result = @client.get_thumbnail_batch ["/img.png"]
+    entry = result.entries.first
+
+    expect(entry.metadata).to be_a(DropboxApi::Metadata::File)
+    expect(entry.thumbnail).to be_a(String)
+    expect(entry.metadata.name).to eq("img.png")
+  end
+
+  it "includes an error entry if the file can't be found", :cassette => "get_thumbnail_batch/not_found" do
+    result = @client.get_thumbnail_batch(["/unknown_file.jpg"])
+    expect(result.entries.first).to be_a(DropboxApi::Errors::NotFoundError)
+  end
+
+  it "raises an argument error with invalid options" do
+    expect {
+      @client.get_thumbnail_batch ["/img.png"], :invalid_arg => "value"
+    }.to raise_error(ArgumentError)
+  end
+end
