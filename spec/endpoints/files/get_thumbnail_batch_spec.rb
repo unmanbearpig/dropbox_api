3b00351af5fbb6e55de01d0d380444e2e71648ed
John Royall <johnroyall7@yahoo.com>
2018-02-13 00:11:33 -0500

    add `build_get_thumbnail_batch` to DropboxScaffoldBuilder
    update GetThumbnailBatch spec to use newly built scaffold

--- spec/endpoints/files/get_thumbnail_batch_spec.rb
+++ spec/endpoints/files/get_thumbnail_batch_spec.rb
@@ -1,10 +1,12 @@
 describe DropboxApi::Client, "#get_thumbnail_batch" do
+  let(:path_prefix) { DropboxScaffoldBuilder.prefix_for :get_thumbnail_batch }
+
   before :each do
     @client = DropboxApi::Client.new
   end
 
   it "returns files", :cassette => "get_thumbnail_batch/success" do
-    result = @client.get_thumbnail_batch ["/img.png"]
+    result = @client.get_thumbnail_batch ["#{path_prefix}/img.png"]
     entry = result.entries.first
 
     expect(entry.metadata).to be_a(DropboxApi::Metadata::File)
@@ -13,13 +15,13 @@ describe DropboxApi::Client, "#get_thumbnail_batch" do
   end
 
   it "includes an error entry if the file can't be found", :cassette => "get_thumbnail_batch/not_found" do
-    result = @client.get_thumbnail_batch(["/unknown_file.jpg"])
+    result = @client.get_thumbnail_batch ["#{path_prefix}/unknown_file.jpg"]
     expect(result.entries.first).to be_a(DropboxApi::Errors::NotFoundError)
   end
 
   it "raises an argument error with invalid options" do
     expect {
-      @client.get_thumbnail_batch ["/img.png"], :invalid_arg => "value"
+      @client.get_thumbnail_batch ["#{path_prefix}/img.png"], :invalid_arg => "value"
     }.to raise_error(ArgumentError)
   end
 end
