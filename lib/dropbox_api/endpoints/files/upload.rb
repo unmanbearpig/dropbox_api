4f845619d25fd62933acaa4b3e47bbe47981a239
Jesús Burgos <jesus@visrez.com>
2018-06-22 16:46:00 +0200

    Updates documentation for upload endpoints

--- lib/dropbox_api/endpoints/files/upload.rb
+++ lib/dropbox_api/endpoints/files/upload.rb
@@ -11,7 +11,7 @@ module DropboxApi::Endpoints::Files
     #
     # Do not use this to upload a file larger than 150 MB.
     #
-    # TODO: Link to `#upload_by_chunks`
+    # For larger files you can use {#upload_by_chunks}.
     #
     # @example
     #   client = DropboxApi::Client.new
@@ -30,16 +30,21 @@ module DropboxApi::Endpoints::Files
     # @param content The contents of the file that will be uploaded. This
     #   could be the result of the +IO::read+ method.
     # @option options mode [DropboxApi::Metadata::WriteMode] Selects what to
-    #   do if the file already exists. The default is +add+.
+    #   do if the file already exists. The default is `add`.
     # @option options autorename [Boolean] If there's a conflict, as determined
-    #   by +mode+, have the Dropbox server try to autorename the file to avoid
-    #   conflict. The default for this field is +false+.
+    #   by `mode`, have the Dropbox server try to autorename the file to avoid
+    #   conflict. The default for this field is `false`.
     # @option options client_modified [DateTime] The value to store as the
-    #   +client_modified+ timestamp. Dropbox automatically records the time at
+    #   `client_modified` timestamp. Dropbox automatically records the time at
     #   which the file was written to the Dropbox servers. It can also record
     #   an additional timestamp, provided by Dropbox desktop clients, mobile
     #   clients, and API apps of when the file was actually created or
     #   modified.
+    # @option options mute [Boolean] Normally, users are made aware of any
+    #   file modifications in their Dropbox account via notifications in the
+    #   client software. If `true`, this tells the clients that this
+    #   modification shouldn't result in a user notification. The default for
+    #   this field is `false`.
     # @see DropboxApi::Metadata::WriteMode
     add_endpoint :upload do |path, content, options = {}|
       validate_options([
