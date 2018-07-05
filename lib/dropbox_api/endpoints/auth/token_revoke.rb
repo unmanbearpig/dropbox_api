e156c02b30d8172139d7d9fdfe85fb34e7f4a724
Brian Henry <brian.henry@delivered.io>
2018-07-05 12:07:13 -0500

    add auth/token/token endpoint

---
+++ lib/dropbox_api/endpoints/auth/token_revoke.rb
@@ -0,0 +1,14 @@
+module DropboxApi::Endpoints::Auth
+  class TokenRevoke < DropboxApi::Endpoints::Rpc
+    Method      = :post
+    Path        = "/2/auth/token/revoke".freeze
+    ResultType  = DropboxApi::Results::VoidResult
+    ErrorType   = nil
+
+    # Revoke the access token for the current account
+    #
+    add_endpoint :token_revoke do
+      perform_request(nil)
+    end
+  end
+end
